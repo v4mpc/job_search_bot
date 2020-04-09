a65e7ed181e510d4daa9720329112d8a05e13e87
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-04-10 00:47:54 +0300

    slack integration is working

--- job_search.py
+++ job_search.py
@@ -7,6 +7,7 @@ import json
 import re
 import time
 import math
+import slack
 
 
 TIME_TO_WAIT=20
@@ -17,6 +18,14 @@ def get_total_jobs():
     sm = html.find(class_="search-main__header__sub-title--faded")
     return int(sm.span.string)
 
+
+def mySlack():
+    slack_token = "xoxb-551750797382-1053373911459-i8k6VH1HHGf702DPGsAYliFj"
+    client = slack.WebClient(token=slack_token)
+    client.chat_postMessage(
+    channel="CG7BZF52B",
+    text="Hello from your app! :tada:"
+    )
 
 def get_total_pages():
     return math.ceil(get_total_jobs()/20)
@@ -61,4 +70,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    mySlack()
