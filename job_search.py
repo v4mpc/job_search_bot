d708f1b3fa2460c9f990a157455e2d7d55129dfd
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-04-10 01:39:23 +0300

    slack notification is working at basic level

--- job_search.py
+++ job_search.py
@@ -19,13 +19,40 @@ def get_total_jobs():
     return int(sm.span.string)
 
 
-def mySlack():
+def mySlack(slack_block):
     slack_token = "xoxb-551750797382-1053373911459-i8k6VH1HHGf702DPGsAYliFj"
     client = slack.WebClient(token=slack_token)
+    template_block=[
+		{
+			"type": "context",
+			"elements": [
+				{
+					"type": "mrkdwn",
+					"text": "*JOBS FOUND*"
+				}
+			]
+		},
+		
+	]
+    for block in slack_block:
+        template_block.append(block)
+    print(template_block)
+
     client.chat_postMessage(
     channel="CG7BZF52B",
-    text="Hello from your app! :tada:"
+    blocks= template_block
     )
+
+def make_block(job,page):
+    return {
+        "type": "context",
+			"elements": [
+				{
+					"type": "mrkdwn",
+					"text": f"_{job}_ @ *page {page}*"
+				}
+			]
+    }
 
 def get_total_pages():
     return math.ceil(get_total_jobs()/20)
@@ -58,17 +85,23 @@ def get_page_html(page=1):
 
 def main():
     print('Begin')
+    jobs=[]
     for page in range(1, get_total_pages()+1):
         time.sleep(TIME_TO_WAIT)
         for job in get_jobs(page):
             if re.search(r'teach(er)?|literature|english|\b(ic?t)\b|software|developer|web|php|system|engineer|programmer|kiswahili|history|civics', job['name'], re.I):
                 # notify_send(f'@page {page}', job["name"])
-                print(job["name"])
+                jobs.append(make_block(job["name"],page))
+                # mySlack(jobs)
+    
+    # send to slack
+    if jobs:
+        mySlack(jobs)
     print('end')
 
 
 
 
 if __name__ == '__main__':
-    # main()
-    mySlack()
+    main()
+    # mySlack()
